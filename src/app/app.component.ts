e5f9b6ff683abd68d58c9d6fb2f75b27e38190a9
SEAjamieD <jamie.degnan@gmail.com>
2019-10-21 08:05:44 -0700

    Basic open and closing menu pusher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'heroku-weather';
+  title = "heroku-weather";
+  menuOpen = false;
+
+  handleOpen(): void {
+    this.menuOpen = !this.menuOpen;
+  }
 }
